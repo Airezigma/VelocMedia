faf5d787672c4d73b0f209ab0364d66fa029e83c
Airezigma <gabs.sfv@gmail.com>
2021-03-28 17:59:05 -0300

    velocidademedia

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace VelocMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia;
+            int tempo;
+            int velocidade;
+
+            Console.Write("Digite seu Valor Distancia: ");
+            distancia = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite seu Valor Tempo: ");
+            tempo = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            velocidade = distancia / tempo;
+            Console.WriteLine("Sua Velocidade Média é = {0} m/s", velocidade);
+            Console.Write("\n");
+
+            Console.WriteLine("Pressione ENTER para sair!");
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
